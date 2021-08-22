e5b92af2c8386a11dd3ecbd6317201a25e1e1a25
Anish Kumar <86920396+anish1737@users.noreply.github.com>
2021-08-22 23:53:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from "./TicTacToe";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+    <TicTacToe/>
+      
+    </div>
+  );
+}
+
+export default App;
